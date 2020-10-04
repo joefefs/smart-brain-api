b93b0443df43e513e2f7df90b4ebe46cfab86d6b
joefefs <design.joseferrer@gmail.com>
2020-10-04 14:42:33 -0500

    connection to psql heroku database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-contoured-02681',
       user : 'postgres',
       password : 'Trauma5.13b',
       database : 'smartbrain'
